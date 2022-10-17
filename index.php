31254e4a610044c0fc3fe9d2904fc846e99abf0b
Matheus Raposo <123lampiao10@gmail.com>
2022-10-17 20:24:52 -0300

    Trabalhando com testes manuais

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+class Slug {
+    private $texto;
+    public function __construct($texto)
+    {
+       $this->texto = $texto; 
+    }
+    public function converter(){
+          
+        $resultado = str_replace(" ","-","$this->texto");// o str_replace() irá subistir um determinado valor que está sendo procurado(1° parâmetro), por outro (2° parâmetro) é uma determinada string,(3° parâmetro)
+        return strtolower($resultado);
+    }
+}
+
+$meuSlug = new Slug("Teste de backend usando PHP");
+echo $meuSlug->converter();
