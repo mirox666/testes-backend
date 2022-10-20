a8d38da389491d47227a7d6317f7204bfcbb2952
Matheus Raposo <123lampiao10@gmail.com>
2022-10-20 20:16:01 -0300

    Finalizado teste backend

--- aula4/test/ClienteTest.php
+++ aula4/test/ClienteTest.php
@@ -3,5 +3,21 @@ use PHPUnit\Framework\TestCase;
 use App\Cliente;
 
 class ClienteTest extends TestCase{
-    
+    public function testVerificarCamposVazios(){
+        $meuCliente = new Cliente("Zilda","Zilda@email.com",123456);
+
+        $this->assertTrue($meuCliente->validaCampos());
+    }
+    public function testContarItensPreferencia(){
+        $meuCliente = new Cliente("Laele","laele@laele.com",456);
+        $meuCliente->setPreferencia(["Promoção","Roupa Barata","Comida Baiana"]);
+
+        $this->assertCount(3,$meuCliente->getPreferencia());//Esse teste irá contar quantos itens existem dentro do vetor.
+    }
+    public function testVerificarTotalCompras(){
+        $meuCliente = new Cliente("Igor","igorrt@hotmail.com",654321);
+        $meuCliente->setTotalCompras(450);
+
+        $this->assertGreaterThan(400,$meuCliente->getTotalCompras());
+    }
 }
